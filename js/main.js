6f067ab6ebd335c563c0279ff7a7c2258559e2af
Brigitte Hofmeister <bhofmei@gmail.com>
2017-09-26 11:50:52 -0400

    formatting

--- js/main.js
+++ js/main.js
@@ -5,67 +5,68 @@ define([
   'dijit/registry',
   'MotifDensityPlugin/View/Dialog/MotifDensityDialog'
   ],
-    function (
-      declare,
-      JBrowsePlugin,
-      dijitButton,
-       dijitRegistry,
-      MotifDensDialog
-    ) {
-      return declare(JBrowsePlugin, {
-          constructor: function (args) {
-            var browser = args.browser;
-            var thisB = this;
+  function (
+    declare,
+    JBrowsePlugin,
+    dijitButton,
+    dijitRegistry,
+    MotifDensDialog
+  ) {
+    return declare(JBrowsePlugin, {
+      constructor: function (args) {
+        var browser = args.browser;
+        var thisB = this;
 
-            // do anything you need to initialize your plugin here
-            this.config.version = '2.1.0';
-            console.log("MotifDensityPlugin plugin starting - v" + this.config.version);
+        // do anything you need to initialize your plugin here
+        this.config.version = '2.1.0';
+        console.log("MotifDensityPlugin plugin starting - v" + this.config.version);
 
-            this.config.dialog = false;
-            if (args.dialogMode !== undefined)
-              this.config.dialog = args.dialogMode;
-            this.config.dialogColors = args.dialogColors || 'random';
-            this.config.dialogMotifs = args.dialogMotifs || ["CG", "CHG", "CHH", "AT"];
+        this.config.dialog = false;
+        if (args.dialogMode !== undefined)
+          this.config.dialog = args.dialogMode;
+        this.config.dialogColors = args.dialogColors || 'random';
+        this.config.dialogMotifs = args.dialogMotifs || ["CG", "CHG", "CHH", "AT"];
 
-            // register track type
-            browser.registerTrackType({
-              label: 'MotifDensity',
-              type: 'MotifDensityPlugin/View/Track/MotifDensity'
+        // register track type
+        browser.registerTrackType({
+          label: 'MotifDensity',
+          type: 'MotifDensityPlugin/View/Track/MotifDensity'
+        });
+        browser.afterMilestone('initView', function () {
+          // create screenshot button (possibly tools menu)
+          var menuBar = browser.menuBar;
+
+          function showMotifDialog() {
+            new MotifDensDialog({
+              setCallback: function (ws, wd, minsc, maxsc, m, clr) {},
+              windowSize: 100,
+              windowDelta: 10,
+              minScore: 0,
+              maxScore: 1,
+              motifs: thisB.config.dialogMotifs,
+              colors: thisB.config.dialogColors
+            }).show();
+          }
+
+          if (thisB.config.dialog) {
+            var button = new dijitButton({
+              innerHTML: 'M',
+              id: 'motif-button',
+              onClick: showMotifDialog
             });
-
-            if (this.config.dialog) {
-              var button;
-              browser.afterMilestone('initView', function () {
-                function showDialog() {
-                  new MotifDensDialog({
-                    setCallback: function (ws, wd, minsc, maxsc, m, clr) {},
-                    windowSize: 100,
-                    windowDelta: 10,
-                    minScore: 0,
-                    maxScore: 1,
-                    motifs: thisB.config.dialogMotifs,
-                    colors: thisB.config.dialogColors
-                  }).show();
-                }
-                // create button
-                button = new dijitButton({
-                  innerHTML: 'M',
-                  title: 'take screen shot of browser',
-                  id: 'motif-button',
-                  onClick: showDialog
-                });
-                browser.menuBar.appendChild(button.domNode);
-              }); // end initview
-
-              browser.afterMilestone('completely initialized', function () {
-                if (browser.view.tracks.length < 1) {
-                  setTimeout(function () {
-                    button = dijitRegistry.byId('motif-button');
-            button.onClick();
-                  }, 1000)
-                }
-              }); // end completely initialized
-            } // end if dialog
-          }// end constructor
-      });
-      });
+            menuBar.appendChild(button.domNode);
+          }
+        });
+        browser.afterMilestone('completely initialized', function () {
+          if (thisB.config.dialog) {
+            if (browser.view.tracks.length < 1) {
+              setTimeout(function () {
+                var button = dijitRegistry.byId('motif-button');
+                button.onClick();
+              }, 700)
+            }
+          }
+        })
+      }
+    });
+  });
