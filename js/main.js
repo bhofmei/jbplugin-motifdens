0dbc25c7bbe4a01483b94b808dba1c5155a23d3e
Brigitte Hofmeister <bhofmei@gmail.com>
2017-09-26 11:38:37 -0400

    fixed dialog mode

--- js/main.js
+++ js/main.js
@@ -1,55 +1,67 @@
 define([
   'dojo/_base/declare',
   'JBrowse/Plugin',
+  'dijit/form/Button',
   'MotifDensityPlugin/View/Dialog/MotifDensityDialog'
   ],
-  function (
-    declare,
-    JBrowsePlugin,
-    MotifDensDialog
-  ) {
-    return declare(JBrowsePlugin, {
-      constructor: function (args) {
-        var browser = args.browser;
-        var thisB = this;
+    function (
+      declare,
+      JBrowsePlugin,
+      dijitButton,
+      MotifDensDialog
+    ) {
+      return declare(JBrowsePlugin, {
+          constructor: function (args) {
+            var browser = args.browser;
+            var thisB = this;
 
-        // do anything you need to initialize your plugin here
-        this.config.version = '2.1.0';
-        console.log("MotifDensityPlugin plugin starting - v" + this.config.version);
+            // do anything you need to initialize your plugin here
+            this.config.version = '2.1.0';
+            console.log("MotifDensityPlugin plugin starting - v" + this.config.version);
 
-        this.config.dialog = false;
-        if (args.dialogMode !== undefined)
-          this.config.dialog = args.dialogMode;
-        this.config.dialogColors = args.dialogColors || 'random';
-        this.config.dialogMotifs = args.dialogMotifs || ["CG", "CHG", "CHH", "AT"];
+            this.config.dialog = false;
+            if (args.dialogMode !== undefined)
+              this.config.dialog = args.dialogMode;
+            this.config.dialogColors = args.dialogColors || 'random';
+            this.config.dialogMotifs = args.dialogMotifs || ["CG", "CHG", "CHH", "AT"];
 
-        // register track type
-        browser.registerTrackType({
-          label: 'MotifDensity',
-          type: 'MotifDensityPlugin/View/Track/MotifDensity'
-        });
+            // register track type
+            browser.registerTrackType({
+              label: 'MotifDensity',
+              type: 'MotifDensityPlugin/View/Track/MotifDensity'
+            });
 
-        // dialog mode
-        browser.afterMilestone('completely initialized', function () {
-          if (thisB.config.dialog) {
-            //if (browser.view.tracks.length < 1) {
-              setTimeout(function () {
-                //var button = dijitRegistry.byId('screenshot-button');
-                //button.onClick();
-                // open dialog
-                new MotifDensDialog({
-                  setCallback: function (ws, wd, minsc, maxsc, m, clr) {},
-                  windowSize: 100,
-                  windowDelta: 10,
-                  minScore: 0,
-                  maxScore: 1,
-                  motifs: thisB.config.dialogMotifs,
-                  colors: thisB.config.dialogColors
-                }).show();
-              }, 700);
-            //}
-          }
-        })
-      }
-    });
-  });
+            if (this.config.dialog) {
+              var button;
+              browser.afterMilestone('initView', function () {
+                function showDialog() {
+                  new MotifDensDialog({
+                    setCallback: function (ws, wd, minsc, maxsc, m, clr) {},
+                    windowSize: 100,
+                    windowDelta: 10,
+                    minScore: 0,
+                    maxScore: 1,
+                    motifs: thisB.config.dialogMotifs,
+                    colors: thisB.config.dialogColors
+                  }).show();
+                }
+                // create button
+                button = new dijitButton({
+                  innerHTML: 'M',
+                  title: 'take screen shot of browser',
+                  onClick: showDialog
+                });
+                browser.menuBar.appendChild(button.domNode);
+              }); // end initview
+
+              browser.afterMilestone('completely initialized', function () {
+                if (browser.view.tracks.length < 1) {
+                  setTimeout(function () {
+                    button.onClick();
+                  }, 700)
+                }
+              }); // end completely initialized
+            } // end if dialog
+          }// end constructor
+      });
+      });
